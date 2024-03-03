b27be77b3ad5cb4ec23a33c1c510ca9cb4bc076e
AgaiGrzes <AgaiGrzes@DESKTOP-MO3UO8L>
2024-03-03 23:01:37 +0100

    Zadanie #15

--- ChallengeAps/Program.cs
+++ ChallengeAps/Program.cs
@@ -2,6 +2,7 @@
 using ChallengeApp;
 
 var employee = new Employee();
+var supervisor= new Supervisor();
 Console.WriteLine(" --------- WItaj w programie do oceny Pracownika ---------");
 Console.WriteLine("****************************************************************");
 
@@ -26,10 +27,40 @@ while (true)
             Console.WriteLine($"Exception : {e.Message}");
         }
     }
+} 
+
+while (true)
+{
+    Console.WriteLine("Podaj ocenę kierownika");
+    var input = Console.ReadLine();
+
+    if (input == "q")
+    {
+        break;
+    }
+
+    else
+    {
+        try
+        {
+            supervisor.AddGrade(input);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Exception : {e.Message}");
+        }
+    }
 }
+
 var statistisc = employee.GetStatistics();
+var statistiscSupervisor=supervisor.GetStatistics();
 Console.WriteLine($"**************** Dane ocen pracownika -> {employee.Name} {employee.Surname} *****************");
 Console.WriteLine($"min: {statistisc.Min}");
 Console.WriteLine($"max: {statistisc.Max}");
 Console.WriteLine($"AVG: {statistisc.Average:n2}");
 Console.WriteLine($"Ocena : {statistisc.AverageLetter}");
+Console.WriteLine($"**************** Dane ocen kierownoika -> {supervisor.Name} {supervisor.Surname} *****************");
+Console.WriteLine($"min: {statistiscSupervisor.Min}");
+Console.WriteLine($"max: {statistiscSupervisor.Max}");
+Console.WriteLine($"AVG: {statistiscSupervisor.Average:n2}");
+Console.WriteLine($"Ocena : {statistiscSupervisor.AverageLetter}");
--- ChallengeAps/Supervisor.cs
+++ ChallengeAps/Supervisor.cs
@@ -1,13 +1,13 @@
 ﻿
 namespace ChallengeApp
 {
-    public class Employee : IEmployee
+
+    public class Supervisor : IEmployee
     {
+
         private List<float> grades = new List<float>();
-
         public string Name => "Jan";
-        public string Surname => "Kowalski";
-
+        public string Surname => "Kierownik";
         public void AddGrade(float grade)
         {
             if (grade >= 0 && grade <= 100)
@@ -16,26 +16,82 @@ namespace ChallengeApp
             }
             else
             {
-               throw new Exception("Invalide grade value");
+                throw new Exception("Invalide grade value");
             }
         }
 
         public void AddGrade(string grade)
         {
-            if (float.TryParse(grade, out float result))
+            switch (grade)
             {
-                this.AddGrade(result);
-            }
-            else if (char.TryParse(grade, out char charResult))
-            {
-                this.AddGrade(charResult);
-            }
+                case "6":
+                    this.AddGrade(100);
+                    break;
+                case "6-" or "-6":
+                    this.AddGrade(95);
+                    break;
 
-            else
-            {
-                throw new Exception("String is not float");
+                case "5":
+                    this.AddGrade(80);
+                    break;
+                case "5-" or "-5":
+                    this.AddGrade(75);
+                    break;
+                case "5+" or "+5":
+                    this.AddGrade(85);
+                    break;
+
+                case "4":
+                    this.AddGrade(60);
+                    break;
+                case "4-" or "-4":
+                    this.AddGrade(55);
+                    break;
+                case "4+" or "+4":
+                    this.AddGrade(65);
+                    break;
+
+                case "3":
+                    this.AddGrade(40);
+                    break;
+                case "3-" or "-3":
+                    this.AddGrade(35);
+                    break;
+                case "3+" or "+3":
+                    this.AddGrade(45);
+                    break;
+
+                case "2":
+                    this.AddGrade(20);
+                    break;
+                case "2-" or "-2":
+                    this.AddGrade(15);
+                    break;
+                case "2+" or "+2":
+                    this.AddGrade(25);
+                    break;
+                case "1":
+                    this.AddGrade(0);
+                    break;
+                default:
+                    if (float.TryParse(grade, out float result))
+                    {
+                        this.AddGrade(result);
+                    }
+                    else if (char.TryParse(grade, out char charResult))
+                    {
+                        this.AddGrade(charResult);
+                    }
+
+                    else
+                    {
+                        throw new Exception("String is not float");
+                    }
+                    break;
             }
         }
+
+
         public void AddGrade(double grade)
         {
             float floatValue = (float)grade;
@@ -48,6 +104,7 @@ namespace ChallengeApp
             float floatValue = grade;
             this.AddGrade(floatValue);
         }
+
         public void AddGrade(char grade)
         {
             switch (grade)
@@ -76,7 +133,7 @@ namespace ChallengeApp
                     break;
 
                 default:
-                    throw new Exception ("Range letters [a-A]-[e-E]");
+                    throw new Exception("Range letters [a-A]-[e-E]");
             }
         }
         public Statistics GetStatistics()
